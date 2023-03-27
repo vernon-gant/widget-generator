d62625c9692c8ca910d44780b4baf27a14d197ac
asy0 <58123184+asy0@users.noreply.github.com>
2023-03-27 17:57:38 +0200

    SignupPage React

--- widget-generator-main/root/Frontend/index.php
+++ widget-generator-main/root/Frontend/index.php
@@ -1,4 +1,3 @@
-
 <head>
     <meta charset="UTF-8">
 
@@ -6,25 +5,137 @@
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-aFq/bzH65dt+w6FI2ooMVUpc+21e0SRygnTpmBvdBgSdnuTN7QbdgL+OapgHtvPp" crossorigin="anonymous" >
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha2/dist/js/bootstrap.bundle.min.js" integrity="sha384-qKXV1j0HvMUeCBQ+QVp7JcfGl760yU08IQ+GpUo5hlbpg51QRiuqHAJz8+BrxE/N" crossorigin="anonymous"></script>
 
-    <link type="text/css" rel="stylesheet" href="./css/registerstyle.css" />
+    <link type="text/css" rel="stylesheet" href="./css/mainstyle.css" />
 
-    <title>Signup</title>
+    <title>Homepage</title>
 
 </head>
 
 
 <body>
 
-
+<header>
  <?php
  include_once 'navbar.php';
  ?>
+</header>
+
+
+
+<section class="blue-bg">
+
+ <hr class="my-4">
+ <div class="container justify-content-center">
+     <div class="row justify-content-center">
+         <div class="frontpage-img text-center">
+             </b><p class="widget-header"><b> WIDGET GENERATOR </b></p> <br><br>
+             <p class="widget-text">
+                 WidgetGenerator is an easy-to-use online tool that lets you create and customize
+                 a wide range of widgets for your website or application. With its extensive
+                 collection of widgets and user-friendly interface, WidgetGenerator makes
+                 it easy to add a personal touch to your digital projects. Give it a try today and see
+                 how WidgetGenerator can help you take your website or app to the next level!
+             </p>
+             <br
+         </div>
+     </div>
+ </div>
+ </div>
+
+    </section>
+
+ <hr>
 
 
 
 
 
 
+ <br><section class= "NEWS">
+     <h1 class="news-header"><b>NEWS</b></h1>
+     <p> Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore. </p>
+     <div class="news">
+         <div class="row">
+             <div class="news-col">
+                 <h3>NEWS I</h3>
+                 <p>Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. </p> <br>
+             </div>
+             <div class="news-col">
+                 <h3>NEWS II</h3>
+                 <p>Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. </p> <br>
+
+             </div>
+             <div class="news-col">
+                 <h3>NEWS III</h3>
+                 <p> Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. </p> <br>
+             </div>
+         </div>
+
+
+         <div class="News-Link">
+             <a href="news.php" role="button" type="button" class="btn btn-outline-secondary">  News  </a>
+         </div> <br>
+
+
+
+
+
+
+
+
+         <section class="blue-bg"><br>
+             <div class="card">
+                 <div class="card-header">
+                     Happy customer says
+                 </div>
+                 <div class="card-body">
+                     <blockquote class="blockquote mb-0">
+                         <p>"WidgetGenerator is the perfect tool for those who want to add a touch of customization and personalization to their website or app. With a wide range of widgets to choose from, WidgetGenerator makes it easy to enhance the functionality and visual appeal of your digital projects."</p>
+                         <footer class="blockquote-footer"> ChatGpt <cite title="Source Title"> </cite></footer>
+                     </blockquote>
+                 </div>
+             </div>
+         </section>
+
+
+
+
+
+
+
+         <br>
+         <section class= "NEWS">
+             <div class="news">
+                 <h2><b>SERVICES</b></h2>
+                 <div class="row">
+                     <div class="news-col">
+                         <div class="icon-wrapper">
+                             <script src="https://cdn.lordicon.com/fudrjiwc.js"></script>
+                             <br><lord-icon src="https://cdn.lordicon.com/yovumtbq.json" trigger="hover" style="width:200px;height:200px">
+                             </lord-icon>
+
+                             <!--<h3><i class="fas fa-wifi fa-4x fa-beat-fade"></i></h3> -->
+
+                         </div>
+                     </div>
+
+                     <div class="news-col">
+                         <div class="icon-wrapper">
+                             <br><br> <h3><i class="fas fa-bell-concierge fa-4x fa-shake"></i></h3>
+                         </div>
+                     </div>
+
+                     <div class="news-col">
+                         <div class="icon-wrapper">
+                             <script src="https://cdn.lordicon.com/fudrjiwc.js"></script>
+                             <br><lord-icon src="https://cdn.lordicon.com/flvisirw.json" trigger="hover" style="width:200px;height:200px"></lord-icon>
+                             <!-- <h3><i class="fas fa-utensils fa-4x fa-beat-fade"></i></h3>  -->
+                         </div>
+                     </div>
+                 </div>
+
+             </div>
+         </section>
 
 
 </body>
