1712baaca925e58da341cf5d955ba1ec3fad0781
Aleksandr Zakharov <aleksza.4119@gmail.com>
2023-04-27 16:41:29 +0200

    Configured logging + improved parser

--- root/backend/app/parsers/reddit_parser.py
+++ root/backend/app/parsers/reddit_parser.py
@@ -1,16 +1,17 @@
 import os
 from datetime import datetime
-from typing import List
 
 import praw
 import prawcore
 from dotenv import load_dotenv
+from praw.reddit import Submission
+from typing import List
 
 from .exceptions import RedditNotFoundError, RedditAPIError
+from flask import current_app as app
 from ..utils import PostResource
 
 load_dotenv()
-
 
 reddit = praw.Reddit(
     client_id=os.environ.get("REDDIT_CLIENT_ID"),
@@ -19,28 +20,56 @@ reddit = praw.Reddit(
 )
 
 
-def is_image_submission(submission) -> bool:
-    image_domains = ['i.redd.it', 'imgur.com']
-    return any(domain in submission.url for domain in image_domains)
+def is_resource(media_metadata: dict, key, resource) -> bool:
+    return str(media_metadata[key]['e']).lower().find(resource) != -1
 
 
-def is_video_submission(submission) -> bool:
+def is_url_submission(submission: Submission) -> bool:
+    return len(str(submission.selftext).strip()) == 0 and \
+        not (is_image_submission(submission) or is_video_submission(submission)) \
+        and submission.url.startswith("http")
+
+
+def is_video_submission(submission: Submission) -> bool:
     video_domains = ['v.redd.it']
     return any(domain in submission.url for domain in video_domains)
 
 
-def get_video_from_submission(submission) -> list:
+def has_embed_videos(submission: Submission) -> bool:
+    return hasattr(submission, 'media_metadata') and submission.media_metadata and \
+        any(is_resource(submission.media_metadata, key, 'video') for key in submission.media_metadata)
+
+
+def get_video_from_submission(submission: Submission) -> list:
     if is_video_submission(submission):
         return [submission.secure_media["reddit_video"]["fallback_url"]]
+    elif has_embed_videos(submission):
+        return [submission.media_metadata[key]['dashUrl']
+                for key in submission.media_metadata
+                if is_resource(submission.media_metadata, key, 'video')
+                ]
     else:
         return []
 
 
-def get_image_from_submission(submission) -> list:
+def is_image_submission(submission: Submission) -> bool:
+    image_domains = ['i.redd.it', 'imgur.com']
+    return any(domain in submission.url for domain in image_domains)
+
+
+def has_embed_images(submission: Submission) -> bool:
+    return hasattr(submission, 'media_metadata') and submission.media_metadata and \
+        any(is_resource(submission.media_metadata, key, 'image') for key in submission.media_metadata)
+
+
+def get_image_from_submission(submission: Submission) -> list:
     if is_image_submission(submission):
         return [image["source"]["url"] for image in submission.preview["images"]]
-    elif hasattr(submission, 'media_metadata') and submission.media_metadata:
-        return [submission.media_metadata[key]['s']['u'] for key in submission.media_metadata]
+    elif has_embed_images(submission):
+        return [submission.media_metadata[key]['s']['u']
+                for key in submission.media_metadata
+                if is_resource(submission.media_metadata, key, 'image')
+                ]
     else:
         return []
 
@@ -49,10 +78,11 @@ def get_reddit_posts(subreddit: str, limit: int = 10) -> List[PostResource]:
     results = []
     # Add limit submissions to the list using reddit
     try:
-        for submission in reddit.subreddit(subreddit).hot(limit=limit):
+        posts = reddit.subreddit(subreddit).hot(limit=limit)
+        for submission in posts:
             results.append(PostResource(
                 title=submission.title,
-                content=submission.selftext,
+                content=submission.selftext if not is_url_submission(submission) else submission.url,
                 author=submission.author.name,
                 # If there are no images in preview, return an empty list
                 images=get_image_from_submission(submission),
@@ -63,12 +93,17 @@ def get_reddit_posts(subreddit: str, limit: int = 10) -> List[PostResource]:
                 id=submission.id,
                 video=get_video_from_submission(submission)
             )._asdict())
+            app.logger.info(f"Successfully processed post {submission.id} - {submission.title} from {subreddit}")
         # Sort by date
         results.sort(key=lambda x: x["date"], reverse=True)
+        app.logger.info(f"Successfully fetched {len(results)} posts from {subreddit}")
         return results
     except prawcore.exceptions.NotFound:
+        app.logger.error(f"Subreddit {subreddit} not found")
         raise RedditNotFoundError(f"Subreddit {subreddit} not found")
     except prawcore.exceptions.Redirect:
+        app.logger.error(f"Subreddit {subreddit} not found")
         raise RedditNotFoundError(f"Subreddit {subreddit} not found")
     except Exception as e:
-        raise RedditAPIError(f"Error fetching Reddit posts: {str(e)}")
+        app.logger.error(f"Failed to fetch Reddit posts from {subreddit}: {e}")
+        raise RedditAPIError(f"Error fetching Reddit posts from {subreddit}: {e}")
