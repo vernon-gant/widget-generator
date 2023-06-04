b4f08fed2a4e14eba96f2c71f6a804b752d32d72
asy0 <58123184+asy0@users.noreply.github.com>
2023-06-04 21:51:16 +0200

    Links wurden aktualisiert

--- root/frontend/src/App.js
+++ root/frontend/src/App.js
@@ -1,13 +1,14 @@
 import React from 'react';
+import { motion } from "framer-motion";
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
-import { Navigation } from './Navba';
+import Navigation from './Navba';
+import AboutUsPage from './About';
+import ImpressPage  from "./Impressum";
 import SearchBox from "./SearchBox";
-import  WidgetList from "./FrequentlyUsedWidgets";
+import WidgetList from "./FrequentlyUsedWidgets";
 import DisplayPosts from "./DisplayPosts";
-import backgroundImage from './img/widget2.png';
-import { motion} from "framer-motion";
 import Posts from './Posts';
-
+import backgroundImage from './img/widget2.png';
 
 
 function App() {
@@ -16,45 +17,43 @@ function App() {
       <Routes>
         <Route path="/" element={
           <>
-              <Navigation />
-              <div className="header-container" style={{
-                  background: `url(${backgroundImage}) no-repeat center center fixed`,
-                  backgroundSize: "cover",
-                  padding: "50px 0",
-                  textAlign: "center",
-                  color: "white"
-                }}>
-                  <motion.span
-                    style={{ display: "inline-block" }}
-                    animate={{ x: [null, 60, 0] }}
-                    transition={{ duration: 5, repeat: 2 }}
+            <Navigation />
+            <div className="header-container" style={{
+                background: `url(${backgroundImage}) no-repeat center center fixed`,
+                backgroundSize: "cover",
+                padding: "50px 0",
+                textAlign: "center",
+                color: "white"
+              }}>
+                <motion.span
+                  style={{ display: "inline-block" }}
+                  animate={{ x: [null, 60, 0] }}
+                  transition={{ duration: 5, repeat: 2 }}
                 >
                   <h1><b>Widget &nbsp;  </b></h1>
                 </motion.span>
 
                 <motion.span
-                    style={{ display: "inline-block" }}
-                    animate={{ y: [null, 60, 0] }}
-                    transition={{ duration: 5, repeat: 2 }}
-                  >
+                  style={{ display: "inline-block" }}
+                  animate={{ y: [null, 60, 0] }}
+                  transition={{ duration: 5, repeat: 2 }}
+                >
                   <h1><b>Generator</b></h1>
                 </motion.span>
 
-
                 <SearchBox />
 
-              </div>
+           </div>
+            <WidgetList />
+            <Posts />
+          </>
+        } />
 
-              <WidgetList />
-               <Posts/>
-            </>
-          } />
-
-          <Route path="/posts" component={DisplayPosts} />
-
+        <Route path="/about" element={<AboutUsPage />} />
+        <Route path="/impress" element={<ImpressPage />} />
+        <Route path="/posts" component={DisplayPosts} />
       </Routes>
     </BrowserRouter>
-
   );
 }
 
