75e77b8dba45888ceb552b3d5e73b73b5f96b645
asy0 <58123184+asy0@users.noreply.github.com>
2023-06-04 21:51:40 +0200

    Navbar Bereich ist fertig

--- root/frontend/src/Navba.js
+++ root/frontend/src/Navba.js
@@ -1,40 +1,56 @@
 import React from 'react';
-import { Navbar, Nav } from 'react-bootstrap';
+import { Navbar, Nav, Container, Row, Col } from 'react-bootstrap';
+import { Link } from 'react-router-dom';
 
+const Navigation = () => {
+  return (
+    <Navbar
+      className="colorNav"
+      variant="light"
+      expand="lg"
+      style={{
+        padding: '20px',
+        backgroundColor: '#9b4db7',
+        color: 'white',
+        top: '0',
+        width: '100%',
+        fontSize: '20px',
+      }}
+    >
+      <Container>
+        <Row>
+          <Col xs={6}>
+            <Link to="/" style={{ textDecoration: 'none' }}>
+              <Navbar.Brand
+                style={{
+                  fontSize: '26px',
+                  color: 'white',
+                  fontWeight: 'bold',
+                  fontFamily: 'Arial, Helvetica, sans-serif',
+                }}
+              >
+                <b>Widget Generator</b>&nbsp;&nbsp;
+              </Navbar.Brand>
+            </Link>
+          </Col>
+          <Col xs={6} className="text-right">
+            <Navbar.Toggle aria-controls="basic-navbar-nav" />
+          </Col>
+        </Row>
+      </Container>
 
-export const Navigation = () => {
-    return (
-        <Navbar classname="colorNav" variant="light" expand="lg" style={
-            {   padding: "20px",
-                backgroundColor: "#9b4db7",
-                color: 'white',
-                top: '0',
-                width: '100%',
-                fontSize: '20px',
-            }
-            }>
+      <Navbar.Collapse id="basic-navbar-nav">
+        <Nav className="ml-auto">
+          <Nav.Link as={Link} to="/about">
+            About
+          </Nav.Link>
+          <Nav.Link as={Link} to="/impress">
+              Impressum
+          </Nav.Link>
+        </Nav>
+      </Navbar.Collapse>
+    </Navbar>
+  );
+};
 
-            <Navbar.Brand href="#home" style={{
-                    fontSize: '26px',
-                    color: 'white',
-                    fontWeight: 'bold',
-                    fontFamily: 'Arial, Helvetica, sans-serif'
-
-                }}>
-                <b> Widget Generator</b>&nbsp;&nbsp;
-            </Navbar.Brand>
-
-            <Navbar.Toggle aria-controls="basic-navbar-nav"/>
-
-            <Navbar.Collapse id="basic-navbar-nav" >
-                <Nav className="mr-auto" >
-                    <Nav.Link href="#about">About</Nav.Link>
-                    <Nav.Link href="#imprint" >Impressum</Nav.Link>
-                </Nav>
-            </Navbar.Collapse>
-
-        </Navbar>
-
-    );
-}
 export default Navigation;
