95d6ab3f5c4a74c224d94eefdf18f86a39c171a7
asy0 <58123184+asy0@users.noreply.github.com>
2023-06-10 01:16:33 +0200

    Back-Button and Styling

--- root/frontend/src/DisplayPosts.js
+++ root/frontend/src/DisplayPosts.js
@@ -1,8 +1,11 @@
-import {useLocation, useParams} from 'react-router-dom';
+import {Link, useLocation, useParams} from 'react-router-dom';
 import Posts from "./Posts";
 import Navigation from "./Navba";
 import React from "react";
-import WidgetList from "./FrequentlyUsedWidgets";
+import {FontAwesomeIcon} from "@fortawesome/react-fontawesome";
+import {faLongArrowLeft} from "@fortawesome/free-solid-svg-icons";
+import './DisplayPosts.css';
+
 
 function DisplayPosts() {
     //console.log("DisplayPosts rendered")
@@ -14,13 +17,17 @@ function DisplayPosts() {
         <div>
             <Navigation/>
             <br/> <br/> <br/> <br/>
+            <Link to="/" element="{<HomePage/>}" role="button">
+                   <FontAwesomeIcon icon={faLongArrowLeft} className="back-button" />
+            </Link>
+
             <div style={{textAlign: 'center'}}>
                 <h1
                     style={{
                         textTransform: 'uppercase',
                         color: '#3c2a64',
                         textShadow: '0 0 3px #f069d9',
-                        textDecoration: 'underline dashed #f069d9',
+                        textDecoration: 'underline #f069d9',
                         fontFamily: 'sans-serif',
                         fontSize: '40px',
                         //boxShadow:'10 10 2px pink',
@@ -32,7 +39,6 @@ function DisplayPosts() {
             <div>
                 <Posts subreddit={subredditName}/>
             </div>
-            <WidgetList/>
         </div>
     );
 }
