7f607e38c2e51fe3e8240b1c1262cf994ea3a26b
asy0 <58123184+asy0@users.noreply.github.com>
2023-06-09 09:59:55 +0200

    Navbar, WidgetList wurde hinzugefügt

--- root/frontend/src/DisplayPosts.js
+++ root/frontend/src/DisplayPosts.js
@@ -1,15 +1,23 @@
 import {useLocation, useParams} from 'react-router-dom';
 import Posts from "./Posts";
+import Navigation from "./Navba";
+import React from "react";
+import WidgetList from "./FrequentlyUsedWidgets";
 
 function DisplayPosts() {
-    console.log("DisplayPosts rendered")
+    //console.log("DisplayPosts rendered")
     let location = useLocation();
     let params = new URLSearchParams(location.search);
     let subredditName = params.get('subreddit');
 
     return (
         <div>
-            <Posts subreddit={subredditName}/>
+          <Navigation />
+            <br /> <br /> <br /> <br />
+            <div>
+                <Posts subreddit={subredditName}/>
+            </div>
+            <WidgetList/>
         </div>
     );
 }
