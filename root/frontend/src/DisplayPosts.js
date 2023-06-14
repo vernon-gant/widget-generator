eafc0494e0f8b601697899767d3bc1bf46c1577a
imoalim <113512259+imoalim@users.noreply.github.com>
2023-06-14 14:10:14 +0200

    The functionality is complete, but the design needs to be finalized.

--- root/frontend/src/DisplayPosts.js
+++ root/frontend/src/DisplayPosts.js
@@ -3,49 +3,55 @@ import Navigation from "./Navba";
 import React from "react";
 import './DisplayPosts.css';
 import FetchPosts from "./FetchPosts";
-import {useSubreddit} from "./Utils";
+import { useSubreddit } from "./Utils";
 import Loader from "./Loader";
 import Error from "./Error";
 import BackButton from "./BackButton";
+import GenerateTag from "./GenerateTag";
 
 
 function DisplayPosts() {
+  const subredditName = useSubreddit();
+  const { posts, loading, error } = FetchPosts({ subreddit: subredditName, numPosts: 10 });
 
-    const subredditName = useSubreddit();
-
-    const {posts, loading, error} = FetchPosts({subreddit: subredditName, numPosts: 10});
-
-    if (loading) return <Loader/>;
-
-    if (error) return <Error error={error}/>;
-
-
+  if (loading) {
     return (
-        <div>
-            <Navigation/>
-            <br/> <br/> <br/> <br/>
-            <BackButton/>
-
-            <div style={{textAlign: 'center'}}>
-                <h1
-                    style={{
-                        textTransform: 'uppercase',
-                        color: '#3c2a64',
-                        textShadow: '0 0 3px #f069d9',
-                        textDecoration: 'underline #f069d9',
-                        fontFamily: 'sans-serif',
-                        fontSize: '40px',
-                        //boxShadow:'10 10 2px pink',
-                    }}>
-                    <b>{subredditName}</b>
-                </h1>
-            </div>
-            <br/> <br/>
-            <div>
-                <Posts posts={posts} loading={loading}/>
-            </div>
-        </div>
+      <div>
+        <Loader />
+      </div>
     );
+  }
+
+  if (error) return <Error error={error} />;
+
+  return (
+    <div>
+      <Navigation />
+      <br /> <br /> <br /> <br />
+      <BackButton />
+
+      <div style={{ textAlign: 'center' }}>
+        <h1
+          style={{
+            textTransform: 'uppercase',
+            color: '#3c2a64',
+            textShadow: '0 0 3px #f069d9',
+            textDecoration: 'underline #f069d9',
+            fontFamily: 'sans-serif',
+            fontSize: '40px',
+            //boxShadow:'10 10 2px pink',
+          }}
+        >
+          <b>{subredditName}</b>
+        </h1>
+      </div>
+      <br /> <br />
+      <div>
+          <GenerateTag />
+        <Posts posts={posts} loading={loading} />
+      </div>
+    </div>
+  );
 }
 
 export default DisplayPosts;
---
+++ root/frontend/src/GenerateTag.js
@@ -0,0 +1,69 @@
+import React, { useState } from "react";
+import { Button, Modal } from "react-bootstrap";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faCopy, faCheck } from "@fortawesome/free-solid-svg-icons";
+
+function GenerateTag() {
+  const [showModal, setShowModal] = useState(false);
+  const [copied, setCopied] = useState(false);
+
+  const handleButtonClick = () => {
+    setShowModal(true);
+  };
+
+  const handleCloseModal = () => {
+    setShowModal(false);
+    setCopied(false); // Reset the copied state when the modal is closed
+  };
+
+  const handleCopyCode = () => {
+    const codeToCopy = `<smedia-widget data-subreddit="${getSubreddit()}"></smedia-widget>
+<script src="http://127.0.0.1:4000/templates/news_wall.js" type="text/javascript" async></script>`;
+    navigator.clipboard.writeText(codeToCopy)
+      .then(() => setCopied(true))
+      .catch((error) => console.error("Error copying code:", error));
+  };
+
+  const getSubreddit = () => {
+    const url = new URL(window.location.href);
+    const subreddit = url.searchParams.get("subreddit");
+    return subreddit || "default"; // Replace "default" with a fallback subreddit if needed
+  };
+
+  return (
+    <div style={{ display: "flex", justifyContent: "center", alignItems: "center" }}>
+      <Button variant={copied ? "success" : "primary"} onClick={handleButtonClick}>
+        <FontAwesomeIcon icon={copied ? faCheck : faCopy} className="mr-2" />
+        {copied ? "Copied!" : "Generate Tag"}
+      </Button>
+
+      <Modal show={showModal} onHide={handleCloseModal}>
+        <Modal.Header closeButton>
+          <Modal.Title>Generate Tag</Modal.Title>
+        </Modal.Header>
+        <Modal.Body>
+          <pre>
+            <code>{`<smedia-widget data-subreddit="${getSubreddit()}"></smedia-widget>
+<script src="http://127.0.0.1:4000/templates/news_wall.js" type="text/javascript" async></script>`}</code>
+          </pre>
+          <Button
+            variant={copied ? "success" : "secondary"}
+            onClick={handleCopyCode}
+            className="mt-3"
+          >
+            <FontAwesomeIcon icon={faCopy} className="mr-2" />
+            {copied ? "Copied!" : "Copy Code"}
+          </Button>
+        </Modal.Body>
+        <Modal.Footer>
+          <Button variant="secondary" onClick={handleCloseModal}>
+            Close
+          </Button>
+          {/* Add any additional buttons or actions here */}
+        </Modal.Footer>
+      </Modal>
+    </div>
+  );
+}
+
+export default GenerateTag;
