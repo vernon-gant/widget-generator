75eef39c59b96a503f343681036a931b89186759
asy0 <58123184+asy0@users.noreply.github.com>
2023-06-16 23:22:26 +0200

    Code-Reformat

--- root/frontend/src/SearchBox.js
+++ root/frontend/src/SearchBox.js
@@ -1,8 +1,8 @@
-import React, { useState } from 'react';
-import { Button, Form, FormControl } from "react-bootstrap";
-import { motion } from "framer-motion";
+import React, {useState} from 'react';
+import {Button, Form, FormControl} from "react-bootstrap";
+import {motion} from "framer-motion";
 import styled from "styled-components";
-import { Link } from "react-router-dom";
+import {Link} from "react-router-dom";
 
 const SearchContainer = styled.div`
   display: flex;
@@ -26,43 +26,43 @@ const SearchButton = styled(motion.button)`
 `;
 
 const SearchBox = () => {
-  const [subreddit, setSubreddit] = useState('');
+    const [subreddit, setSubreddit] = useState('');
 
-  const handleInputChange = (event) => setSubreddit(event.target.value);
+    const handleInputChange = (event) => setSubreddit(event.target.value);
 
-  const handleKeyDown = (event) => {
-    if (event.key === 'Enter') {
-      event.preventDefault();
-      window.location.href = `/reddit?subreddit=${subreddit}`;
-    }
-  };
+    const handleKeyDown = (event) => {
+        if (event.key === 'Enter') {
+            event.preventDefault();
+            window.location.href = `/reddit?subreddit=${subreddit}`;
+        }
+    };
 
-  return (
-    <SearchContainer>
-      <Form inline>
-        <FormControl
-          id="search"
-          type="text"
-          placeholder=" Enter Subreddit"
-          className="mr-sm-4"
-          value={subreddit}
-          onChange={handleInputChange}
-          onKeyDown={handleKeyDown} // Handle Enter key press
-          style={{
-            textAlign: "center",
-            borderRadius: "40px",
-            width: "280px",
-            height: "50px",
-          }}
-        />
-        <br /> <br />
-        <Link to={`/reddit?subreddit=${subreddit}`}>
-          <Button variant="outline-info">Generate</Button>
-        </Link>
-      </Form>
-      <br />
-    </SearchContainer>
-  );
+    return (
+        <SearchContainer>
+            <Form inline>
+                <FormControl
+                    id="search"
+                    type="text"
+                    placeholder=" Enter Subreddit"
+                    className="mr-sm-4"
+                    value={subreddit}
+                    onChange={handleInputChange}
+                    onKeyDown={handleKeyDown} // Handle Enter key press
+                    style={{
+                        textAlign: "center",
+                        borderRadius: "40px",
+                        width: "280px",
+                        height: "50px",
+                    }}
+                />
+                <br/> <br/>
+                <Link to={`/reddit?subreddit=${subreddit}`}>
+                    <Button variant="outline-info">Generate</Button>
+                </Link>
+            </Form>
+            <br/>
+        </SearchContainer>
+    );
 };
 
 export default SearchBox;
